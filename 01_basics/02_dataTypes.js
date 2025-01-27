141c995f9063b9a59eba130b582f47561b612191
divya shukla <divyashukla2913@gmail.com>
2025-01-27 14:58:18 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// Primitive datatypes
+
+// number => range of 2 to power 52
+// bigint => max number value
+// string => ""
+// boolean => true/false
+// null => standalone value empty value
+// undefined => 
+// symbol => unique
+
+
+// non primitive datatypes
+
+// object
+
+console.log(typeof "string") // string
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
