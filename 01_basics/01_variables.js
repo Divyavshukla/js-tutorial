dee109f17364caccf74873d0b66a190ca611021e
divya shukla <divyashukla2913@gmail.com>
2025-01-27 14:32:37 +0530

    variables notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144557
+let accountEmail = 'divyashukla2913@gmail.com'
+var accountPassword = '12345'
+accountCity = 'Jaipur'
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "divi@gmail.com"
+accountPassword = "212121212"
+accountCity = "Bengaluru"
+
+/*
+
+prefer not to use var
+because of issues in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
