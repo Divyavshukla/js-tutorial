241f619ef13d1e836784b16f604a7ababa456802
divya shukla <divyashukla2913@gmail.com>
2025-01-31 10:27:03 +0530

    basic of array

--- 01_basics/dataSummary.js
+++ 01_basics/dataSummary.js
@@ -35,3 +35,50 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "divyagirl@gmail.com";
+
+let anothername = myYoutubename; // here the copy is used
+anothername = "chaiaurcode";
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: 'user@google.com', // this will also update
+    upi: "user@ybl",
+}
+
+let userTwo = userOne; // reference
+
+console.log(userTwo);
+
+userTwo.email = "abc@gmail.com";
+
+
+
+// In stack primitive value are stored and in heap non-primitive value are stored 
+
+// Stack:
+// ----
+
+// Think of it like a stack of plates: You can only add or remove the top plate.
+// Used for: Temporary storage, like function calls and local variables.
+// Automatic: Memory is automatically allocated when a function starts and freed when it ends.
+// Fast: Accessing data is quick because it’s organized neatly.
+// Limited size: If you use too much (like too many plates), it can overflow.
+
+
+// Heap:
+// ----
+
+// Think of it like a messy room: You can put things anywhere and take them out whenever you want.
+// Used for: Long-term storage, like objects and data that need to stick around.
+// Manual: You have to remember to clean up (free the memory) when you’re done.
+// Slower: Accessing data can be slower because it’s not organized.
+// Larger size: You can use more memory, but you need to manage it carefully.
+// In short, the stack is fast and automatic but limited, while the heap is flexible and larger but requires more management.
